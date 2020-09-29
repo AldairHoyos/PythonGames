6663b280b6385886c86f746d81472ffb77f905bf
AldairHoyos <aldairdejesushoyostorres@gmail.com>
2020-09-29 14:49:44 -0400

    A way to control the amount of guesses that a player can do was included, nevertheless we haven't included a way to ask the player if he/she desires to bet again and therefore play again.

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -19,28 +19,26 @@ class Player:
     
 def WelcomeMessage():
     print("Welcome to our humble place of amusement")
-    time.sleep(1)
-    print("\nWe want to offer you a simple game but fun and profitable")
     time.sleep(2)
+    print("\nWe want to offer you a simple game but fun and profitable")
+    time.sleep(3)
     print("\nThe game consists on guessing a number between 1 and 20 and you will have six tries to guess it")
     time.sleep(3)
-    print("\nIf you guess the number you'll win twice the money you bet but...")
-    time.sleep(2)
-    print("\nOn the other hand, if you loose, you loose the money you bet")
-    time.sleep(2)
+    print("\nIf you guess the number you'll win twice the money you bet but on the other hand, if you loose, you loose the money you bet")
+    time.sleep(4)
     print("\nDo you want to enter and play?")
     print("\n1. Yes, it would be a placer to do so")
     print("\n2. No, I'm okey but thanks a lot!")
     while True:
         resp=int(input("Answer from stranger: "))
         if resp not in set([1,2]):
-            print("\nSorry but you have to give us a right answer fo this, thre's no middle ground")
+            print("\nSorry but you have to give us a right answer for this, thre's no middle ground")
         else:
             break
     if resp==2:
-        print("\nit's such a shame that you've decided that, I hope you change your mind any time soon")
+        print("\nIt's such a shame that you've decided that, I hope you change your mind any time soon")
     else:
-        print("Perfect! You have taken a great choice, my friend")
+        print("\nPerfect! You have taken a great choice, my friend")
     return resp
     
 def GuessTheNumber():
@@ -56,24 +54,36 @@ def GuessTheNumber():
         player=Player(name,6,bet)
         hiddennumber=random.randint(1, 21)
         while player.tries > 0:
-            cont=0
+            print("\nYou have",player.tries,"tries to guess the number")
             while True:
                 guess=int(input("Choose a number between 1 and  20: "))
                 if guess < 1 or guess > 20:
-                    print("\nSelect a number between the range shown")
+                    print("\nSelect a number between the valid range")
                 else:
+                    player.options.append(guess)
                     break
             if hiddennumber == guess:
                 print("\nWe have a winner! You have guessed the right number")
-                print("\nYou have won",bet,"dollars")
+                print("\nYou have won",player.bet,"dollars")
+                break
             else:
-                cont=cont+1
-                print("\nSorry but you have selected the wrong guess")
-                if hiddennumber < guess:
-                    print("\nThe real number is lower than you might think")
+                player.tries=player.tries-1
+                time.sleep(1)
+                print("\nSorry but you have made a wrong guess")
+                if player.tries > 0:
+                    if hiddennumber < guess:
+                        time.sleep(1)
+                        print("\nThe real number is lower than you might think")
+                    else:
+                        time.sleep(1)
+                        print("\nThe real number is higher than you might think")
                 else:
-                    print("\nThe real number is higher than you might think")
-            
+                    time.sleep(1)
+                    print("\nYou have run out of guesses, my friend")
+                    time.sleep(1)
+                    print("\nThe number that we chose for you was: ",hiddennumber)
+                    time.sleep(1)
+                    print("\nYou have lost",player.bet,"dollars")                    
             
         
     
