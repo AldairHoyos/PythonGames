57bbe2c0467eff82e5dc30ff01e4244c943def38
AldairHoyos <aldairdejesushoyostorres@gmail.com>
2020-09-30 14:59:40 -0400

    A function that especifacally controls the decision making of any player that has played the game was included (This function was part of another that controls the game itself but this time they were separated from each other)
    Besides the previous commentary, all unnecessary lines were deleted and
    bugs were corrected.

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -8,7 +8,6 @@ Created on Tue Sep 29 07:48:51 2020
 import random
 import time
 
-
 class Player:
     def __init__(self,name,tries,bet):
         self.name=name
@@ -16,31 +15,44 @@ class Player:
         self.bet=bet
         self.options=list()
   
-    
 def WelcomeMessage():
-    print("Welcome to our humble place of amusement")
+    print("Welcome to our humble place of amusement.")
     time.sleep(2)
-    print("\nWe want to offer you a simple game but fun and profitable")
+    print("\nWe want to offer you a simple game but fun and profitable.")
     time.sleep(3)
-    print("\nThe game consists on guessing a number between 1 and 20 and you will have six tries to guess it")
+    print("\nThe game consists on guessing a number between 1 and 20 and you will have six tries to guess it.")
     time.sleep(3)
-    print("\nIf you guess the number you'll win twice the money you bet but on the other hand, if you loose, you loose the money you bet")
+    print("\nIf you guess the number you'll win twice the money you bet but on the other hand, if you loose, you loose the money you bet.")
     time.sleep(4)
     print("\nDo you want to enter and play?")
-    print("\n1. Yes, it would be a placer to do so")
+    print("\n1. Yes, it would be a placer to do so.")
     print("\n2. No, I'm okey but thanks a lot!")
+    time.sleep(2)
     while True:
         resp=int(input("Answer from stranger: "))
         if resp not in set([1,2]):
-            print("\nSorry but you have to give us a right answer for this, thre's no middle ground")
+            print("\nSorry but you have to give us a right answer for this, thre's no middle ground.")
+            time.sleep(1.5)
         else:
             break
     if resp==2:
-        print("\nIt's such a shame that you've decided that, I hope you change your mind any time soon")
+        print("\nIt's such a shame that you've decided that, I hope you change your mind any time soon.")
     else:
-        print("\nPerfect! You have taken a great choice, my friend")
+        print("\nPerfect! You have taken a great choice, my friend.")
     return resp
-    
+
+def PlayAgain():
+    while True:
+        print("\nAre you interested in playing again the game?")
+        print("\n1. Yes, I'm interested.")
+        print("\n2. No, I want to go home.")
+        resp=int(input("Answer: "))
+        if resp not in set([1,2]):
+            print("\nSelect a valid option, please.")
+        else:
+            break
+    return resp
+
 def GuessTheNumber():
     resp=WelcomeMessage()
     if resp==1:
@@ -48,57 +60,51 @@ def GuessTheNumber():
         while True:
             bet=float(input("Tell us how much do you want to bet on this game: "))
             if bet <= 0:
-                print("\nYou cannot bet with magic money, my friend. Give us a real amount")
+                print("\nYou cannot bet with magic money, my friend. Give us a real amount.")
             else:
                 break
         player=Player(name,6,bet)
-        hiddennumber=random.randint(1, 21)
+        hiddennumber=random.randint(1, 20)
         while player.tries > 0:
-            print("\nYou have",player.tries,"tries to guess the number")
+            print("\nYou have",player.tries,"tries to guess the number.")
             while True:
                 guess=int(input("Choose a number between 1 and  20: "))
                 if guess < 1 or guess > 20:
-                    print("\nSelect a number between the valid range")
+                    print("\nSelect a number between the valid range.")
                 else:
                     player.options.append(guess)
                     break
             if hiddennumber == guess:
-                print("\nWe have a winner! You have guessed the right number")
-                print("\nYou have won",player.bet,"dollars")
+                print("\nWe have a winner! You have guessed the right number.")
+                print("\nYou have won",player.bet,"dollars.")
             else:
                 player.tries=player.tries-1
                 time.sleep(1)
-                print("\nSorry but you have made a wrong guess")
+                print("\nSorry but you have made a wrong guess.")
                 if player.tries > 0:
                     if hiddennumber < guess:
                         time.sleep(2)
-                        print("\nThe real number is lower than you might think")
+                        print("\nThe real number is lower than you might think.")
+                        time.sleep(2)
                     else:
                         time.sleep(2)
-                        print("\nThe real number is higher than you might think")
+                        print("\nThe real number is higher than you might think.")
+                        time.sleep(2)
                 else:
                     time.sleep(2)
-                    print("\nYou have run out of guesses, my friend")
+                    print("\nYou have run out of guesses, my friend.")
                     time.sleep(2)
                     print("\nThe number that we chose for you was: ",hiddennumber)
                     time.sleep(2)
-                    print("\nYou have lost",player.bet,"dollars")                    
+                    print("\nYou have lost",player.bet,"dollars.")                    
                     time.sleep(2)
             if hiddennumber == guess or player.tries == 0:
-                while True:
-                    print("\nAre you interested in playing again the game?")
-                    print("\n1. Yes, I'm interested")
-                    print("\n2. No, I want to go home")
-                    resp=int(input("Answer: "))
-                    if resp not in set([1,2]):
-                        print("\nSelect a valid option, please")
-                    else:
-                        if resp==1:
-                            print("\nNicely done, my friend. You're the kind of guy for this game")
-                            player.tries=6
-                            break
-                        else:
-                            break
+                resp=PlayAgain()
+                if resp==1:
+                    print("\nNicely done, my friend. You're the kind of guy for this game.")
+                    player.tries=6
+                else:
+                    break
         
     
       
