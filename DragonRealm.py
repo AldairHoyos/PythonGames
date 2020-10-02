326b2dcb14bbb4ba3d80975e0dcb9f0d9dc66514
AldairHoyos <aldairdejesushoyostorres@gmail.com>
2020-10-02 19:24:22 -0400

    The game has been completed in functionality. everything that was supposed to do is being done.
    We added the final function that allows the user playing DragonRealm.

--- DragonRealm.py
+++ DragonRealm.py
@@ -9,12 +9,13 @@ import random
 import time
 
 class Dragon:
-    self.name="Smaug"
-    self.color="Green"
-    self.age="Two centuries"
+    def __init__(self):
+        self.name="Smaug"
+        self.color="Green"
+        self.age="Two centuries"
     
     def presentation(self):
-        return "\nWho dares to awake me from my everlasting dream?\n I am",self.name,"Dragon of all Dragons!\n You interrupted my dream!"
+        print(f"\nWho dares to awake me from my everlasting dream? I am {self.name}, Dragon of all Dragons! You interrupted my dream and for that, you must pay with your life!")
     
     def attacks(self):
         attack=random.randint(1,3)
@@ -27,11 +28,12 @@ class Dragon:
         
 
 def WarningMessage():
-    print("You are in a land full of dragons. In front of you, you see two caves.",end="")
+    print("Wizard: Hello and welcome, you lost traveler! You have entered to a dangerous land.")
+    print("\nYou are in Dragons Land. In front of you, you see two caves.",end="")
     time.sleep(3)
-    print(" In one cave there is endless treasure that will make you rich instantly.",end="")
+    print(" In one cave there are endless treasures that will make you rich instantly.",end="")
     time.sleep(4)
-    print(" Nevertheless, inside the other cave, there is a greedy and hungry dragon that can eat you alive without hesitation if it sees you.",end="")
+    print(" Nevertheless, inside the other cave, there is a greedy and hungry dragon that can eat you alive without hesitation if it sees you.")
     while True:
         print("\nDo you want to take a chance and enter to one of the caves and see if you're lucky one way or the other?")
         print("\n1. Yes, fortune rewards risky people! I have nothing to fear.")
@@ -44,5 +46,27 @@ def WarningMessage():
     return answ
 
 def DragonRealm():
-    
-    
+    answ=WarningMessage()
+    if answ == 1:
+        print("\nAlright wonderer, you have to decide which cave you want to enter to")
+        while True:
+            print("\nChoose a cave from the followings:")
+            print("\n1. Left cave")
+            print("\n2. Right cave")
+            answ=int(input("Decision: "))
+            if answ not in set([1,2]):
+                print("\nThere is not middle ground on this one, my friend.")
+            else:
+                break
+        cave=1
+        if answ==cave:
+            dragon=Dragon()
+            dragon.presentation()
+            time.sleep(4)
+            print(f"\n{dragon.name} attack: " + dragon.attacks())
+            time.sleep(2)
+            print("\nLuck was not with you! You have lost your life in exchage of greed!")
+        else:
+            print("\nYou lucky wonderer! You have made yourself rich!\nYou have found the the most valuable treasure of all time!")
+    else:
+        print("\nIn that case, you have nothing to do in here, go back where you came from and come back only when you remeber where your guts were!")
