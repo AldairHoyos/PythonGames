fbc8aae32941dde65f15a3ab42ea3098511f6056
AldairHoyos <aldairdejesushoyostorres@gmail.com>
2020-10-14 13:58:57 -0400

    We just added to our Hangman game two different classes that will separate the two players of this game, namely, the one that hides a word and the other one that guesses the word letter by letter until such player wins or gets hanged.
    Each class contains a method, one for choosing a word (For the one
    that hides the letter) and another one for guessing letter by letter
    (For the one that's playing to save its neck)

--- Hangman.py
+++ Hangman.py
@@ -4,3 +4,47 @@ Created on Wed Oct 14 08:07:16 2020
 
 @author: Aldair Hoyos
 """
+import string
+
+class Hanger():
+    def __init__(self,name):
+        self.name=name
+    
+    def hangerword(self):
+        while True:
+            word=list(input(f"{self.name}´s hidden word: "))
+            val=True
+            for i in range(len(word)):
+                if word[i] not in string.ascii_letters:
+                    val=False
+            if val==False:
+                print("\nPlease, choose a valid word of the english language")
+            else:
+                print("\nNice pick, my friend")
+                break
+        self.word=word
+        
+    
+class HangedMan():
+    def __init__(self,name,chances):
+        self.name=name
+        self.chances=chances
+    
+    def hangedguess(self):
+        while True:
+            while True:
+                letter=input(f"{self.name}´s guess letter: ")
+                if len(letter) > 1:
+                    print("\nRemember that you should guess the word letter by letter and not the entire word at once")
+                else:
+                    break
+            if letter not in string.ascii_letters:
+                print("\nPlease, choose a valid letter of the english language")
+            else:
+                break
+        return letter
+                        
+            
+    
+        
+        
