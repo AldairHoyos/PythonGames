b99c006e152773f887d7fcb56b8a2d9d895b752e
AldairHoyos <aldairdejesushoyostorres@gmail.com>
2020-10-14 17:22:19 -0400

    The following updates were included in the developing of this game:
    We included a method for the player that allows us to create the underscore blacks depending on the length of the word hidden, this is done automatically.
    
    A welcoming message was also included as a function. This was done in order to  provide some sense to anyone who might use this program and it will allow that person to see the rules of this game.
    
    Lastly, a function called "CheckingResult" was included and it will allow us to determine if a letter provided by a player is or is not part of the hidden word.

--- Hangman.py
+++ Hangman.py
@@ -5,14 +5,15 @@ Created on Wed Oct 14 08:07:16 2020
 @author: Aldair Hoyos
 """
 import string
+import time
 
 class Hanger():
-    def __init__(self,name):
-        self.name=name
+    def __init__(self):
+        self.name="The Hanger"
     
     def hangerword(self):
         while True:
-            word=list(input(f"{self.name}´s hidden word: "))
+            word=input(f"{self.name}'s hidden word: ")
             val=True
             for i in range(len(word)):
                 if word[i] not in string.ascii_letters:
@@ -24,16 +25,21 @@ class Hanger():
                 break
         self.word=word
         
-    
 class HangedMan():
-    def __init__(self,name,chances):
-        self.name=name
-        self.chances=chances
+    def __init__(self):
+        self.name="Player"
+        self.chances=5
+        self.lista=list()
+    
+    def fillblanks(self,word):
+        for i in range(len(word)):
+            self.lista.append("_")
+    
     
     def hangedguess(self):
         while True:
             while True:
-                letter=input(f"{self.name}´s guess letter: ")
+                letter=input(f"{self.name}'s guess letter: ")
                 if len(letter) > 1:
                     print("\nRemember that you should guess the word letter by letter and not the entire word at once")
                 else:
@@ -43,8 +49,39 @@ class HangedMan():
             else:
                 break
         return letter
-                        
-            
     
-        
-        
+def CheckingResult(hanger,hanged,letter):
+    while True:
+        if letter in hanger.word:
+            print("\nYou have guessed  a letter successfully")
+            hanged.lista.append(letter)
+        else:
+            print("\nThe letter you chose is not part of the hidden word")
+            hanged.chances=hanged.chances-1
+
+def WelcomingMessage():
+    print('\nWelcome to "The Hangman Game".')
+    time.sleep(2)
+    print("\nIn this game someone will radonmly indicate a word for you to guess.")
+    time.sleep(3)
+    print("\nConsequently, you will try to guess the entire word by indicating a letter for such word.")
+    time.sleep(4)
+    print("\nIf the indicated letter appears in the word, it will be displayed for you.")
+    time.sleep(4)
+    print("\nNevertheless, if you don't guess any letter in the word, you loose a point.")
+    time.sleep(3)
+    print("\nYou have a total of 5 points with you to try to guess the entire word.")
+    time.sleep(3)
+    print("\nIf you loose all your points, you loose the game, but if you guess the word, you win!")
+    time.sleep(3)
+    while True:
+        print("\nDo you want to play the game?")
+        print("\n1. Yes, I definetely want to play!")
+        print("\n2. No, I have better things to do with my life.")
+        answ=int(input("Answer from player: "))
+        if answ not in set([1,2]):
+            print("\nPlease, select one of the possible options.")
+            return False
+        else:
+            break
+    return answ
