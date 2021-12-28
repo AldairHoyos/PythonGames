a447ab031d8c8e0c056c335c54eacbd771639465
AldairHoyos <aldairdejesushoyostorres@gmail.com>
2021-12-28 14:15:13 -0400

    Hemos llevado a cabo el desarrollo de la primera version completa del juego. Realizamos varios cambios y añadimos algunas funcionalidades a las clases involucradas, estas son:
    1) Añadimos un método a la clase "Persona" para la cual disponemos de un
       constructor alternativo adaptado al juego per se.
    
    2) Convertimos a las variables win y lose en variables de la clase para
       que de esta forma podamos asociarla a un método de la clase llamado
       "repeat_menu" donde evaluaremos si volvemos a jugar o no, en caso de
       que la respuesta sea positiva, las variables de la clase deben
       reiniciarse y estas se reinician en dicho método.
    
    3) La clase "comparar" fue modificada, las variables para coontrolar las
       victorias dejaron de depender de esta función.
    
    4) El juego "rock_paper_scissors" fue finalizado por completo (1ra
       versión) Tenemos un controlador para el tipo de modalidad de juego,
       para decidir si se vuelve a jugar o no y, contamos con un controlador
       para las inversiones del juego.

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -2,7 +2,7 @@
 """
 Created on Mon Dec 27 10:47:03 2021
 
-@author: Uso Personal
+@author: Aldair Hoyos
 """
 
 class Person():
@@ -10,11 +10,25 @@ class Person():
         self.user = prenom
         self.money = argent
     
+    #Una representación alternativa para crear una clase persona que este adaptada al juego per se.
+    @classmethod
+    def fromPerson(cls):
+        print("\nEn lo siguiente nos indicará sus datos, por favor")
+        name = input("Nombre completo o alias: ")
+        while True:
+            try:
+                money = float(input("Dinero a apostar: "))
+                if money <= 0:
+                    print("\nIngrese una cantidad válida para jugar")
+                else:
+                    break
+            except:
+                print("\nLa cantidad debe expresarla numéricamente, por favor")
+        return cls(name,money)
+    
 class Game():
-    def __init__(self, jouer1: Person):
-        self.player = jouer1
-        self.win = 0
-        self.lose = 0
+    win = 0
+    lose = 0
     
     @staticmethod
     def choise():
@@ -35,26 +49,45 @@ class Game():
     
     @staticmethod
     def menu():
-        print("Indiquenos la versión del juego que desea jugar:")
+        print("\nAhora, indiquenos la versión del juego que desea jugar:")
         print("\n1. El mejor a un solo intento")
         print("\n2. El mejor a tres intentos")
         print("\n3.El mejor a cinco intentos")
         while True:
             try:
-                resp = int(input("Respuesta del juegador: "))
+                resp = int(input("Respuesta del jugador: "))
                 if resp not in {1,2,3}:
                     print("\nSeleccione una opción válida, por favor.")
                 else:
                     break
             except:
-                print("\nLa respuesta dada no tiene sentido, seleccione uno de los valores mostrados, por favor.")
+                print("\nLa respuesta dada no tiene sentido, seleccione uno de los opciones mostrados, por favor.")
         return resp
     
-    def comparar(self, x, y):
+    @classmethod
+    def repeat_menu(cls):
+        print("\n¿Desea volver a jugar?")
+        print("\n1. Si, quiero jugar de nuevo")
+        print("\n2. No, ya fue suficiente")
+        while True:
+            try:
+                resp = int(input("Respuesta del jugador: "))
+                if resp not in {1,2}:
+                    print("\nSeleccione una opción válida, por favor.")
+                else:
+                    cls.win, cls.lose = 0, 0
+                    break
+            except:
+                print("\nLa respuesta dada no tiene sentido, seleccione uno de las opciones mostrados, por favor.")
+        return resp
+    
+    @classmethod
+    def comparar(cls, opcion, x, y):
+        #Realizamos las comparaciones entre las manos de los jugadores y contabilizamos las victorias o derrotas
         if (x == 1 and y == 3) or (x == 2 and y == 1) or (x == 3 and y == 2):
-            self.win += 1
+            cls.win += 1
         elif (y == 1 and x == 3) or (y == 2 and x == 1) or (y == 3 and x == 2):
-            self.lose += 1
+            cls.lose += 1
         else:
             pass
     
@@ -91,20 +124,45 @@ class Game():
     def rock_paper_scissors(self):
         import random
         import time
-        print(f"Bienvenido {self.player.user} al juego de piedra, papel o tijeras")
-        opcion = Game.menu()
-        while self.win != opcion and self.lose != opcion:
-            self.player.hand = Game.choise()
-            cpu = random.randint(1,3)
-            print("\nPiedra, ",end="")
-            time.sleep(1)
-            print("papel o ",end="")
-            time.sleep(1)
-            print("tijeras",end="")
-            Game.comparar(self, self.player.hand, cpu)
-            Game.show_results(self.player.hand, cpu)
-        if self.win > self.lose:
-            print(f"\nVictoria para {self.player.user}")
-        else:
-            print(f"\nDerrota para {self.player.user}")
+        print("¡Bienvenido al juego de piedra, papel o tijeras!")
+        self.player = Person.fromPerson()
+        while True:
+            opcion = Game.menu()
+            while Game.win != opcion and Game.lose != opcion:
+                self.player.hand = Game.choise()
+                cpu = random.randint(1,3)
+                print("\nPiedra, ",end="")
+                time.sleep(1)
+                print("papel o ",end="")
+                time.sleep(1)
+                print("tijeras",end="")
+                Game.comparar(opcion, self.player.hand, cpu)
+                Game.show_results(self.player.hand, cpu)
+                time.sleep(1)
+            if Game.win > Game.lose:
+                time.sleep(1)
+                self.player.money*=(2*opcion)
+                print(f"\nVictoria para {self.player.user}")
+                print(f"\nAhora usted posee la cantidad de {self.player.money} dólares")
+                time.sleep(1)
+            else:
+                time.sleep(1)
+                self.player.money = 0
+                print(f"\nDerrota para {self.player.user}")
+                print("\nAhora usted se quedó sin dinero...")
+                time.sleep(1)
+            replay = Game.repeat_menu()
+            if replay == 1:
+                print("\nVolvamos a jugar")
+                if self.player.money == 0:
+                    try:
+                        self.player.money = float(input("Ingrese un nuevo monto a apostar: "))
+                        if self.player.money <= 0:
+                            print("\nIngrese una cantidad válida para jugar")
+                    except:
+                        print("\nLa cantidad debe expresarla numéricamente, por favor")
+            else:
+                break
+        print("\nGame over...")
+            
             
